1b6aa00123cb5dbf839abf692a70a361a40af3ca
kisslaci98 <klaci9898@gmail.com>
2019-11-12 14:19:29 +0100

    Masodik projekt elso commitja

---
+++ Masodik_projekt/Masodik_projekt/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Masodik_projekt
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
