cb4751f96a29146261626c322d8bf5f51243ec3d
kisslaci98 <klaci9898@gmail.com>
2019-11-12 15:09:15 +0100

    Masodik projekt masodik commitja

--- Masodik_projekt/Masodik_projekt/Program.cs
+++ Masodik_projekt/Masodik_projekt/Program.cs
@@ -10,6 +10,65 @@ namespace Masodik_projekt
     {
         static void Main(string[] args)
         {
+            /*Console.WriteLine("1-től 100-ig azok a számok, amelyek valódi osztóinak az összege osztható 7-tel.");
+
+            for (int i = 1; i < 101; i++)
+            {
+                if (osszeghettel(i))
+                {
+                    Console.WriteLine($"{i} valódi osztóinak összege osztható 7-tel");
+                }
+            }
+            
+
+            Console.ReadKey(true);*/
+
+            Console.WriteLine("2-től 100-ig a prímszámokat: ");
+
+            for (int i = 2; i < 101; i++)
+            {
+                if (primszamE(i))
+                {
+                    Console.WriteLine(i);
+                }
+            }
+
+            Console.ReadKey(true);
+        }
+        static bool osszeghettel(int szam)
+        {
+            int osszeg = 0;
+            bool hettel = true;
+
+            for (int i = 1; i < szam; i++)
+            {
+                if (szam % i == 0)
+                {
+                    osszeg += i;
+                }
+            }
+            if  (osszeg % 7 != 0)
+            {
+                hettel = false;
+            }
+
+            return hettel;
+        }
+        static bool primszamE(int szam)
+        {
+            bool primE = true;
+            int hatar = (int)Math.Floor(Math.Sqrt(szam)) + 1;
+
+            for (int i = 2; i < hatar; i++)
+            {
+                if (szam % i == 0)
+                {
+                    primE = false;
+                    break;
+                }
+            }
+
+            return primE;
         }
     }
 }
